f0ace71a598d33a8904feef62feb8826bad2f826
Samuel Cornet <corsam28@gmail.com>
2023-10-25 13:36:01 -0500

    Implement the UI for the authentication

---
+++ client/readconnect/src/pages/login.tsx
@@ -0,0 +1,12 @@
+import Page from "@/components/Page/Page";
+import SignIn from "@/components/Authentication/SignIn/SignIn";
+
+const LogIn = () => {
+  return (
+    <Page>
+      <SignIn />
+    </Page>
+  );
+};
+
+export default LogIn;
---
+++ client/readconnect/src/pages/register.tsx
@@ -0,0 +1,12 @@
+import Page from "@/components/Page/Page";
+import SignUp from "@/components/Authentication/SignUp/SignUp";
+
+const Registration = () => {
+  return (
+    <Page>
+      <SignUp />
+    </Page>
+  );
+};
+
+export default Registration;
